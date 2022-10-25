4cf3fe9a3555af61b73c3f8b841a65346090178f
Mudi26 <s215772@student.dtu.dk>
2022-10-25 18:43:56 +0200

    Added to My_Profile composable

--- app/src/main/java/com/example/sensimate/MyProfile.kt
+++ app/src/main/java/com/example/sensimate/MyProfile.kt
@@ -1,10 +1,35 @@
 package com.example.sensimate
 
+import android.graphics.Color
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
+@Preview
 @Composable
 fun My_Profile(){
+    //Add billedet "profilepicture"
+    Box (
+        modifier = Modifier
+            .width(392.dp)
+            .height(205.dp)
+            .clip(RoundedCornerShape(topStart = 0.dp, topEnd = 0.dp, bottomStart = 0.dp, bottomEnd = 0.dp))
+//            .background(R.drawable.mypoints)
+            .padding(start = 0.dp, top = 0.dp, end = 0.dp, bottom = 0.dp)
+            .alpha(1f)
+    ) {
+    }
+
+
 }
 @Composable
 fun PointsGained(){
@@ -13,20 +38,5 @@ fun PointsGained(){
 fun PointReward(){
 }
 @Composable
-fun MenuBar(){
-}
-@Composable
 fun EditProfile(){
-
 }
-
-@Preview
-@Composable
-fun My_ProfilePreview(){
-    My_Profile()
-    PointsGained()
-    PointReward()
-    MenuBar()
-    EditProfile()
-}
-
