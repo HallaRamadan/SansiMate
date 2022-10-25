33f629f0e7f94405fc4e66013d9193028586d8f2
HallaRamadan <s193731@>
2022-10-25 11:04:00 +0200

    "ADD FILE"

--- app/src/main/java/com/example/sensimate/MainActivity.kt
+++ app/src/main/java/com/example/sensimate/MainActivity.kt
@@ -3,12 +3,17 @@ package com.example.sensimate
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.sensimate.ui.theme.SensiMateTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,31 +24,16 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background) {
-                    SensiMateApp()
+
 
                 }
             }
         }
     }
 }
-@Preview
-@Composable
-fun SensiMatePreview() {
-    SensiMateApp()
-
-}
 
 
 
-@Composable
-fun SensiMateApp(){
 
-}
-@Composable
-fun MenuBarApp(){
 
-}
-@Composable
-fun SenSiMateIcon(){
 
-}
