ed3a234108cfa657aa2f02bc83aebe7a224b7486
KingBendico <cph-bc105@cphbusiness.dk>
2022-10-25 18:37:23 +0200

    values->strings.xml added example of strings for drawables

--- app/src/main/java/com/example/sensimate/Discover.kt
+++ app/src/main/java/com/example/sensimate/Discover.kt
@@ -1,16 +1,45 @@
 package com.example.sensimate
 
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.material.Text
 /* -------- Composable that to display Disover screen  -------*/
+
 @Composable
 fun Discover(){
-
+    ProductList()
 
 }
+
+@Composable
+fun ProductList() {
+
+    LazyColumn {
+        // Add a single item
+        item {
+            Text(text = "First item")
+        }
+
+        // Add 5 items
+        items(5) { index ->
+            Text(text = "Item: $index")
+        }
+
+        // Add another single item
+        item {
+            Text(text = "Last item")
+        }
+    }
+}
+
+
+
 @Preview
 @Composable
 fun DiscoverPreview(){
     Discover()
 
 }
+
+
--- app/src/main/java/com/example/sensimate/data/Datasource.kt
+++ app/src/main/java/com/example/sensimate/data/Datasource.kt
@@ -14,14 +14,12 @@
  * limitations under the License.
  */
 package com.example.sensimate.data
-import com.example.affirmations.R
-import com.example.affirmations.model.Affirmation
 
 /**
  * [Datasource] generates a list of [Affirmation]
  */
 class Datasource() {
-    fun loadAffirmations(): List<Affirmation> {
+/*    fun loadAffirmations(): List<Affirmation> {
         return listOf<Affirmation>(
             Affirmation(R.string.affirmation1, R.drawable.image1),
             Affirmation(R.string.affirmation2, R.drawable.image2),
@@ -33,5 +31,5 @@ class Datasource() {
             Affirmation(R.string.affirmation8, R.drawable.image8),
             Affirmation(R.string.affirmation9, R.drawable.image9),
             Affirmation(R.string.affirmation10, R.drawable.image10))
-    }
+    }*/
 }
