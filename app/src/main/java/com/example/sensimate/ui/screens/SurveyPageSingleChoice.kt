465d6a1a48bae9d995dbe61a184128f9d58f2cc4
miladibra1 <s215796@student.dtu.dk>
2022-10-25 20:02:35 +0200

    SurveyPageSingleChoice layout

--- app/src/main/java/com/example/sensimate/ui/screens/SurveyPageSingleChoice.kt
+++ app/src/main/java/com/example/sensimate/ui/screens/SurveyPageSingleChoice.kt
@@ -1,10 +1,36 @@
 package com.example.sensimate.ui.screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.RadioButton
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 
+class Answer {
+
+    val text: String
+}
+
 @Composable
-fun SurveyPage_SingleChoice(){
+fun SurveyAnswer(answer: Answer) {
+    Row() {
+        Text(answer.text)
+        RadioButton(selected =, onClick = { /*TODO*/ })
+    }
+}
+
+@Composable
+fun SurveyPage_SingleChoice(answers: List<Answer>){
+    Column() {
+        answers.forEach { answer ->
+            SurveyAnswer(
+                answer = answer,
+                isselected = false
+            )
+        }
+
+    }
 
 }
 @Preview
