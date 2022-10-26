26e342428de7fc6fc836d27c93e94899814430c3
miladibra1 <s215796@student.dtu.dk>
2022-10-26 22:49:39 +0200

    SurveyPageSingleChoice

--- app/src/main/java/com/example/sensimate/ui/screens/SurveyPageSingleChoice.kt
+++ app/src/main/java/com/example/sensimate/ui/screens/SurveyPageSingleChoice.kt
@@ -1,39 +1,75 @@
 package com.example.sensimate.ui.screens
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import android.graphics.Color
+import android.graphics.Color.GRAY
+import android.widget.GridLayout
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.RadioButton
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.TopCenter
+import androidx.compose.ui.graphics.Color.Companion.Gray
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-
-class Answer {
-
-    val text: String
-}
-
-@Composable
-fun SurveyAnswer(answer: Answer) {
-    Row() {
-        Text(answer.text)
-        RadioButton(selected =, onClick = { /*TODO*/ })
-    }
-}
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.sensimate.R
+import java.time.format.TextStyle
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
 
 @Composable
-fun SurveyPage_SingleChoice(answers: List<Answer>){
-    Column() {
-        answers.forEach { answer ->
-            SurveyAnswer(
-                answer = answer,
-                isselected = false
-            )
+fun SurveyPage_SingleChoice(){
+
+        Column(modifier = Modifier.padding(0.dp)) {
+            Box(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .heightIn(min = 120.dp)
+                    .fillMaxWidth(1F)
+                    .border(BorderStroke(1.dp, Gray), RoundedCornerShape(20.dp)),
+                contentAlignment = Alignment.CenterStart,
+
+
+
+                ) {
+
+                Row(horizontalArrangement = Arrangement.spacedBy(45.dp),
+                    modifier = Modifier
+                        .fillMaxHeight(1F)
+                        .fillMaxWidth(1F)
+                ) {
+                    Column(verticalArrangement = Arrangement.spacedBy(5.dp)) {
+                        Text(text = "This is a question, dont question\nit, just answer it*")
+                    }
+
+                }
+            }
+
+
+
+
         }
-
     }
 
-}
-@Preview
+
+/*
+Text(
+    text = "This is a question, dont question\nit, just answer it *",
+    style = androidx.compose.ui.text.TextStyle(
+        fontSize = 16.sp,
+        fontWeight = FontWeight.Bold
+    )
+)
+}*/
+
+@Preview (showBackground = true)
 @Composable
 fun SurveyPage_SingleChoicePreview(){
     SurveyPage_SingleChoice()
