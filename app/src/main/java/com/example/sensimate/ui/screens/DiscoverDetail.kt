e065445cca58bbae980a0ae4129c722a508ffc52
Mudi26 <s215772@student.dtu.dk>
2022-10-26 21:51:03 +0200

    Added UI to MyProfile.kt

--- app/src/main/java/com/example/sensimate/ui/screens/DiscoverDetail.kt
+++ app/src/main/java/com/example/sensimate/ui/screens/DiscoverDetail.kt
@@ -1,20 +1,52 @@
 package com.example.sensimate.ui.screens
 
+import android.view.ViewDebug.CapturedViewProperty
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.sensimate.R
+import com.example.sensimate.ui.theme.SensiMateTheme
+
+
+/*@Preview
+@Composable
+fun DiscoverDetailPreview(){
+    DiscoverDetail()
+}
+@Composable
+fun DiscoverDetail() {
+    DiscoverDetailCard {
+        DrinkEvent()
+    }
+    DiscoverDetailCard {
+        Allergens()
+    }
+    DiscoverDetailCard {
+        MillionBar()
+    }
+    DiscoverDetailCard {
+        Map()
+    }
+}
+
+ */
+
+
+
+
 
 
 @Preview
@@ -48,6 +80,7 @@ fun Discover_DetailPreview() {
                 ) {
                     Text(text = "Allergens\n" + "N/A")
                 }
+
                 Box(
                     modifier = Modifier
                         .fillMaxWidth(1F)
@@ -100,28 +133,88 @@ fun DiscoverDetailCard (content: @Composable () -> Unit) {
         }
     }
 
-/*@Composable
-fun BackSave(){
-CardTitle(title = "Back")
+
+/*
+@Composable
+fun DrinkEvent() {
+    Column(verticalArrangement = Arrangement.spacedBy(0.1.dp)) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth(1F)
+                .fillMaxHeight(0.25F)
+                .padding()
+                .alpha(0.85f)
+                .background(Color.White)
+        ) {
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(10.dp),
+                modifier = Modifier.padding(3.dp)
+            ) {
+                Text(text = "Beer From Goldstar\n" + "21/10/2022\n" + "20 places left")
+                Image(painterResource(R.drawable.hand_beer), "content description")
+            }
+        }
+    }
+}
+
+@Composable
+fun Allergens() {
+    Column(verticalArrangement = Arrangement.spacedBy(0.1.dp)) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth(1F)
+                .fillMaxHeight(0.33F)
+                .padding()
+                .alpha(0.85f)
+                .background(Color.White),
+        ) {
+            Text(text = "Allergens\n" + "N/A")
+        }
+        }
 
 }
 
 @Composable
-fun eventDetail(){
-    CardTitle(title = "Beer from Goldstar")
-
+fun MillionBar(){
+    Column(verticalArrangement = Arrangement.spacedBy(0.1.dp)) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth(1F)
+                .fillMaxHeight(0.15F)
+                .padding()
+                .alpha(0.85f)
+                .background(Color.White)
+        ) {
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(80.dp),
+                modifier = Modifier.padding(3.dp)
+            ) {
+                Text(text = "Million Bar")
+                Text(text = "1km")
+            }
+        }
+    }
 }
 
 @Composable
-fun Allergens(){
-    CardTitle(title = "Allergens")
-
-}
-
-@Composable
-fun Map(){
-    CardTitle(title = "Million Bar")
-
+fun Map() {
+    Column(verticalArrangement = Arrangement.spacedBy(0.1.dp)) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth(1F)
+                .fillMaxHeight(1F)
+                .padding()
+                .alpha(0.85f)
+                .background(Color.White)
+        ) {
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(100.dp),
+                modifier = Modifier.padding(3.dp)
+            ) {
+                Image(painterResource(R.drawable.mapdiscover), "content discription")
+            }
+        }
+    }
 }
 
  */
