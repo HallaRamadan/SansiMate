c7733217a13aa41fca8d74e3ad333900f8491b21
Christoffer Foldager <c.k.foldager@gmail.com>
2023-01-06 13:59:37 +0100

    redid buttomnavgraph

--- app/src/main/java/com/example/sensimate/navigation/BottomNavGraph.kt
+++ app/src/main/java/com/example/sensimate/navigation/BottomNavGraph.kt
@@ -7,7 +7,9 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.example.sensimate.navigation.BottomBarScreen
+import com.example.sensimate.navigation.Screen
 import com.example.sensimate.ui.screens.Discover
+import com.example.sensimate.ui.screens.EventDetails
 import com.example.sensimate.ui.screens.MyEvent
 
 
@@ -22,9 +24,14 @@ fun BottomNavGraph(navController: NavHostController) {
         composable(route = BottomBarScreen.Home.route) {
             MyEvent()
         }
-        composable(route = BottomBarScreen.Discover.route) {
+
+        composable(route = Screen.EventDetails.route) {
+            EventDetails()
+        }
+        composable(route = Screen.Discover.route) {
             Discover(navController)
         }
+
     }
 }
 
