be5870c4eb4fa42f0f51fd1311647018a9f215cc
Christoffer Foldager <c.k.foldager@gmail.com>
2023-01-09 12:59:16 +0100

    added comments about how the JSON would look like

--- app/src/main/java/com/example/sensimate/model/Question.kt
+++ app/src/main/java/com/example/sensimate/model/Question.kt
@@ -1,5 +1,20 @@
 package com.example.sensimate.model
 
+/*
+JSON:
+
+{
+    SurveyID: "The uniq ID given to the survey, by firestore",
+    Type: 2,
+    Title: "some Title",
+    SubTitle: "Som subtitle"
+}
+
+The ID of the question objects, is the ID that firestore gives the document. This ID should
+be stored under the attribute ID in the Question Object.
+ */
+
+
 class Question {
     //TODO: Create Questions class
 }
