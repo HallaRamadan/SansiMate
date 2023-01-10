6819c1da7d2a8586de3f114122c822b465c3f8a3
Christoffer Foldager <c.k.foldager@gmail.com>
2023-01-10 11:28:44 +0100

    Started on function

--- app/src/main/java/com/example/sensimate/model/FirestoreUtils.kt
+++ app/src/main/java/com/example/sensimate/model/FirestoreUtils.kt
@@ -1,13 +1,26 @@
 package com.example.sensimate.model
 
+import android.util.Log
+
 fun RetriveAndParseSurveyJson(surveyID: String):String {
     //TODO: Connects to the firestore, and retrives a survay based on ID
     //      Returns a JSON Strings
     return JsonString
 }
 
-fun RetriveAndParseQuestionsJson(SurveyID: String):Array<String> {
+fun RetriveAndParseQuestionsJson(surveyID: String):Array<String> {
     //TODO: Connects to the firestore, and retrives questions based on ID
     //      Returns an Array of JSON Strings
+    db.collection("questions")
+        .whereEqualTo("surveyId", surveyID)
+        .get()
+        .addOnSuccessListener { documents ->
+            for (document in documents) {
+                Log.d(TAG, "${document.id} => ${document.data}")
+            }
+        }
+        .addOnFailureListener { exception ->
+            Log.w(TAG, "Error getting documents: ", exception)
+        }
     return [JsonStrings]
 }
