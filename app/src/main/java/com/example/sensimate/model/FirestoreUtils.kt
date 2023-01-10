cd13c0e6ebf42be32c3bad1e1c4494606bd3672c
Christoffer Foldager <c.k.foldager@gmail.com>
2023-01-10 11:02:14 +0100

    Function is done. needs Firestore singleton

--- app/src/main/java/com/example/sensimate/model/FirestoreUtils.kt
+++ app/src/main/java/com/example/sensimate/model/FirestoreUtils.kt
@@ -1,9 +1,25 @@
 package com.example.sensimate.model
 
-fun RetriveAndParseSurveyJson(surveyID: String):String {
+import android.util.Log
+
+fun RetriveAndParseSurveyJson(surveyID: String):Survey? {
     //TODO: Connects to the firestore, and retrives a survay based on ID
     //      Returns a JSON Strings
-    return JsonString
+    val docRef = db.collection("surveys").document(surveyID)
+    var surveyObject: Survey? = null
+
+    docRef.get()
+        .addOnSuccessListener { document ->
+            if (document != null) {
+                surveyObject = document.toObject<Survey>()
+            } else {
+                Log.d("Firestore onSuccess", "No such document")
+            }
+        }
+        .addOnFailureListener { exception ->
+            Log.d("Firestore onFailure", "get failed with ", exception)
+        }
+    return surveyObject
 }
 
 fun RetriveAndParseQuestionsJson(SurveyID: String):Array<String> {
