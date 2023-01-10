656241b8b54ef1102ed7770fea6867268b5ddc84
Mudi26 <s215772@student.dtu.dk>
2023-01-10 13:36:11 +0100

    ViewModel

--- app/src/main/java/com/example/sensimate/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/sensimate/viewmodel/MainViewModel.kt
@@ -1,8 +1,5 @@
 package com.example.sensimate.viewmodel
 
-import androidx.lifecycle.ViewModel
-import com.example.sensimate.model.Survey
-
 // class SurvayViewModel : ViewModel() {
     // val _SurvayID:String =TODO()
    // val Survey: Survey = getEventSurvay(_SurvayID)
