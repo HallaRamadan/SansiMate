3f2fc3df4eeed470a8d88a1169c49efb87b33667
Mudi26 <s215772@student.dtu.dk>
2023-01-10 13:35:30 +0100

    ViewModel

--- app/src/main/java/com/example/sensimate/viewmodel/SurvayViewModel.kt
+++ app/src/main/java/com/example/sensimate/viewmodel/SurvayViewModel.kt
@@ -15,3 +15,64 @@ import com.example.sensimate.model.Survey
   //  }
 
 // }
+
+
+
+
+
+
+
+
+
+
+
+/*
+package com.example.sensimate.viewmodel
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+
+class MainViewModel {
+    fun mainView(){
+
+        //logic
+        var currentUser: User
+        // Perform login and determine the current user
+        val isAdmin = login()
+        currentUser = if (isAdmin) Admin() else GuestUser()
+
+        //view with the help of jetpack compose
+        val view = @Composable {
+            when(currentUser) {
+                is GuestUser() -> showGuestUserView()
+                is Admin() -> showAdminView()
+            }
+        }
+
+        Compose.setConent { view }
+
+
+    }
+}
+
+fun login(){
+    val user: Int
+    val Admin: Int
+
+
+    ChoiceOfLogin = when
+
+}
+
+
+fun ChoiceOfLogin(){
+    var choiceOfLogin = mutableStateOf(null)
+    if (choiceOfLogin == 1){
+
+
+
+    }
+}
+
+
+ */
