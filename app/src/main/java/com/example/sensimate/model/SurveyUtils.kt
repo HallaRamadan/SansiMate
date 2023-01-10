f4e24fa326206cc67be9e06b86565bfd4fae2935
Christoffer Foldager <c.k.foldager@gmail.com>
2023-01-10 10:25:50 +0100

    changes to TODO

--- app/src/main/java/com/example/sensimate/model/SurveyUtils.kt
+++ app/src/main/java/com/example/sensimate/model/SurveyUtils.kt
@@ -1,19 +1,6 @@
 package com.example.sensimate.model
 
 
-fun parseSurveyJson(JsonString: String): Survey {
-    //TODO: Takes in the Json string from firestore, and parses it to survayClass
-    //      This function might also call parseQuestiJson
-    //      Returns a Survay object
-    return SurvayObject
-}
-
-fun parseQuestionsJson(JsonString: String): Array<Question> {
-    //TODO: Takes in Json String from firestore, and parses it to QuestionClass
-    //      Returns an Array of Question objects
-    return [QuestionObject]
-}
-
 fun fetchSurvay(SurveyId: String):String {
     //TODO: Calls the function in FirestoreUtils, that retrives a survay from the firestore
     //      Returns a Json String
