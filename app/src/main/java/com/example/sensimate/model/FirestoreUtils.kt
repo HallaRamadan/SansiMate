db8a96ef432085e3a63856cd5f5eb878e9fcceb7
Christoffer Foldager <c.k.foldager@gmail.com>
2023-01-10 11:30:13 +0100

    almost done

--- app/src/main/java/com/example/sensimate/model/FirestoreUtils.kt
+++ app/src/main/java/com/example/sensimate/model/FirestoreUtils.kt
@@ -1,6 +1,7 @@
 package com.example.sensimate.model
 
 import android.util.Log
+import java.util.Collections.list
 
 fun RetriveAndParseSurveyJson(surveyID: String):String {
     //TODO: Connects to the firestore, and retrives a survay based on ID
@@ -11,6 +12,8 @@ fun RetriveAndParseSurveyJson(surveyID: String):String {
 fun RetriveAndParseQuestionsJson(surveyID: String):Array<String> {
     //TODO: Connects to the firestore, and retrives questions based on ID
     //      Returns an Array of JSON Strings
+    val questionsArray: Array<Question> = list()
+
     db.collection("questions")
         .whereEqualTo("surveyId", surveyID)
         .get()
